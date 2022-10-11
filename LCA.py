d1dfc648fdb8149f4cf93c73506654b5eb94bd86
abhilash2121 <61039521+abhilash2121@users.noreply.github.com>
2022-10-11 11:35:34 +0530

    Update LCA.py

--- LCA.py
+++ LCA.py
@@ -4,7 +4,7 @@ class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
         if not root:
             return None
-        if root.data == p or root.data == q:
+        if root.val == p or root.val == q:
             return root
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
