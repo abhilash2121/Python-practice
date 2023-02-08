7098f56b1fb51e0bc8c1d8b7a8431a3e3188351f
Abhilash_Mandaloju <Fazer@12345>
2023-02-08 19:10:23 +0530

    new commit

--- AWStest.py
+++ AWStest.py
@@ -1,14 +1,17 @@
 import boto3
 from boto3.dynamodb.conditions import Attr, Key
 
+abc = False
 dynamodb = boto3.resource('dynamodb', region_name="eu-west-2")
 table = dynamodb.Table("job_details")
 
 # scan_kwargs = {"FilterExpression": Attr("model_version_name").eq("risk_auto_sum_1.0.1")}
 
-params = {"KeyConditionExpression": Key("job_id").eq("100c")}
-response = table.query(**params)
+params = {"FilterExpression": Key("model_version_name").eq("risk_auto_sum_1.0.1") &
+          Key("ground_truth_records_processed").eq(0)}
+response = table.scan(**params)
 
 result = response["Items"]
 
-print(result[0]["model_version_name"])
+for i in result:
+    print(i)
